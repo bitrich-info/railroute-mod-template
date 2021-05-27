0594b552abdb95e85e085c923ef4b2b5cea7e12f
Angel <zdenek.dolezal@gmail.com>
2021-05-27 17:31:03 +0200

    Initial mod template

---
+++ README.md
@@ -0,0 +1,7 @@
+WIP
+
+# Rail Route Mod Template
+
+Game: https://go.bitrich.info/RailRouteSteam
+
+Put RailRoute.dll into `/lib` directory.
---
+++ src/MyMod.cs
@@ -0,0 +1,18 @@
+﻿using Game;
+using Game.Mods;
+using UnityEngine;
+
+namespace Mod {
+    /// <summary>
+    /// Mod Entry point, must extend `ScriptableObject` and implement `IGameMod`.
+    /// </summary>
+    public class MyMod : ScriptableObject, IGameMod {
+        public void OnInit(IGameController gameController) {
+            Debug.Log("[NewMod] => OnInit");
+        }
+
+        public void OnRestart() {
+            Debug.Log("[NewMod] => OnRestart");
+        }
+    }
+}
